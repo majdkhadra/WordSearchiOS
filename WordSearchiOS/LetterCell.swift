fddb8feee04379d5cca39e6e01619a1dc68733a0
Majd <m3khadra@gmail.com>
2020-09-23 11:33:19 -0400

    Update 1

---
+++ WordSearchiOS/LetterCell.swift
@@ -0,0 +1,18 @@
+//
+//  LetterCell.swift
+//  WordSearchiOS
+//
+//  Created by Majd Khadra on 2020-09-05.
+//  Copyright © 2020 majd khadra. All rights reserved.
+//
+
+import UIKit
+
+class LetterCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
